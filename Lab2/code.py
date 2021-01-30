b9888d10584b0edb59f618bddd68a750627016fc
Ammar Sharbat <ammarsharbat@Ammars-MacBook-Air.local>
2021-01-29 22:39:31 -0500

    added lookups(), append() and their csvs

--- Lab2/code.py
+++ Lab2/code.py
@@ -1,10 +1,62 @@
 import timeit
 import csv
 
-def copy_test():
-    l1 = [1, 2, 3, 4, 5, 6, 7]
-    l2 = l1.copy()
-    print("l1 copy: ", l2)
+
+list2 = []
+for i in range(1000000):
+    list2.append(0)
 
 
-print("-".join(str(n) for n in range(201)))
+def lookups(n):
+    return list2[n]
+
+def lookups_test():
+    csvfile=open('lookups.csv','w', newline='')
+    obj=csv.writer(csvfile)
+    temp = 0
+
+    start = timeit.default_timer()
+    for i in range(1000000):
+            
+        lookups(i)
+            
+        if(i / 500 > 0 and i % 500 == 0):
+            end = timeit.default_timer()
+            temp = end - start
+            obj.writerow((i, temp))
+
+    csvfile.close()
+
+
+list3 = []
+
+
+def append(n):
+    list3.append(n)
+
+def append_test():
+    csvfile=open('append.csv','w', newline='')
+    obj=csv.writer(csvfile)
+    temp = 0
+
+    start = timeit.default_timer()
+    for i in range(1000000):
+            
+        append(i)
+
+        if(i / 500 > 0 and i % 500 == 0):
+            end = timeit.default_timer()
+            temp = end - start
+            obj.writerow((i, temp))
+
+    csvfile.close()
+
+
+
+lookups_test()
+
+append_test()
+
+
+d = {"name":"Ammar", "age":"20", "marks":"L"}
+
