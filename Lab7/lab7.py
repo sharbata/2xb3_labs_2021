efab3981cdd39be8c1b1ac870fec38b8182bcd44
kallaym@mcmaster.ca <yoloming02@gmail.com>
2021-03-12 22:13:32 -0500

    lab

--- Lab7/lab7.py
+++ Lab7/lab7.py
@@ -25,6 +25,24 @@ class Graph:
     def number_of_nodes():
         return len()
 
+ex = Graph(7)
+print(ex.adj)
+ex.add_edge(0, 1)
+print(ex.adj)
+ex.add_edge(1, 2)
+ex.add_edge(1, 3)
+ex.add_edge(2, 4)
+ex.add_edge(4, 3)
+ex.add_edge(4, 5)
+ex.add_edge(6, 2)
+print(ex.adj)
+
+
+#q = deque([0])
+#print(q)
+#q.popleft()
+#print(q)
+
 
 #Breadth First Search
 def BFS(G, node1, node2):
@@ -43,6 +61,42 @@ def BFS(G, node1, node2):
                 marked[node] = True
     return False
 
+print(BFS(ex, 0 , 6))
+
+def BFS2(G, node1, node2):
+    if not BFS(G, node1, node2):
+        return []
+    
+    
+    
+    Q = deque([node1])
+    marked = {node1 : True}
+    for node in G.adj:
+        if node != node1:
+            marked[node] = False
+    path = []
+    for node in G.adj:
+        if node == node1:
+            path.append(node)
+    while len(Q) != 0:
+        current_node = Q.popleft()
+        for node in G.adj[current_node]:
+            if node == node2:
+                path.append(node)
+                #print(path)
+                return path
+            if not marked[node]:
+                Q.append(node)
+                marked[node] = True
+                if node not in path:
+                    path.append(node)
+            #print(Q)
+            #print(marked)
+    #return False
+
+print(BFS2(ex, 0, 6)) 
+print(BFS2(ex, 0, 9))
+print(BFS2(ex, 0, 0))
 
 #Depth First Search
 def DFS(G, node1, node2):
