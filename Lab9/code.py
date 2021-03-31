bf8f271732e5a418408852f747f2f831d8895cf1
Ammar Sharbat <sharbata@mcmaster.ca>
2021-03-30 23:11:54 -0400

    basic timetest

--- Lab9/code.py
+++ Lab9/code.py
@@ -1,1 +1,27 @@
+from shortest_paths import *
+from lab9 import *
+import timeit
+import math
+import random
+
+def timetest(runs, size, graph, prim):
+    total = 0
+    for _ in range(runs):
+        G = graph(size)
+        start = timeit.default_timer()
+        prim(G)
+        end = timeit.default_timer()
+        total += end - start
+    return total/runs
+
+def timing_output(algo_1, algo_2, lower, upper, increment):
+    algo_1_runtime = 0
+    algo_2_runtime = 0
+    for _ in range(lower, upper, increment):
+        algo_1_runtime = timetest(10, _, create_random_complete_graph, algo_1)
+        algo_2_runtime = timetest(10, _, create_random_complete_graph, algo_2)
+
+        print(str(_) + "\t" + str(algo_1_runtime) + "\t" + str(algo_2_runtime))
+
+timing_output(bellman_ford_approx, bellman_ford, )
 
